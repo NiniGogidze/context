a7a873d9b6cc270b31b2863738a0d27b6470525c
NiniGogidze <119741770+NiniGogidze@users.noreply.github.com>
2023-07-05 16:02:50 +0400

    create context

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import "./pages/style.css";
+import { MyContext } from "./MyContext";
+import React, { useState, useEffect } from 'react';
+
+import Home from './pages/Home';
+import About from './pages/About';
+
 
 function App() {
+  const [theme, setTheme] = useState('light');
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+  };
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <MyContext.Provider>
+      <div className='topnav'>
+        <h2>re:educate</h2>
+        <Link to="/" className='link'>home</Link>
+        <Link to="/about" className='link'>about</Link>
+        <button onClick={toggleTheme}>DARK MODE</button>
+      </div>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About />}/>
+      </Routes>
+      </MyContext.Provider>
+      
     </div>
   );
 }
 
 export default App;
+
